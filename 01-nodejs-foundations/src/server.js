933783fe9aa33056832c1dc9f6d968f335fb3328
Raphael Marques <raphaeljcmarques@gmail.com>
2023-09-19 20:14:00 -0300

    nodejs foundations

--- 01-nodejs-foundations/src/server.js
+++ 01-nodejs-foundations/src/server.js
@@ -1,5 +1,29 @@
 import http from 'node:http'
 
-const server = http.createServer((req, res) => res.end('Hello World'))
+const users = []
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@gmail.com'
+    })
+
+    return res
+      .writeHead(201)
+      .end()
+  }
+
+  return res.writeHead(404).end()
+})
 
 server.listen(3333)
